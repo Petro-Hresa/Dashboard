5bb73777d96234718cdc71873ff67a3dd023cc3d
Petro-Hresa <gresapetro379@gmail.com>
2022-10-26 14:51:34 +0300

    deploy build project

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports =  {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('css-mqpacker'),
-        (process.env.NODE_ENV === 'production' ?  require('cssnano')  :  null),
-    ]
-}
