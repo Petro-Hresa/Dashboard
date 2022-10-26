90fdfa75e4d21ce4e9c3d0da36262738db367ac6
Petro-Hresa <gresapetro379@gmail.com>
2022-10-26 15:09:36 +0300

    add dashboard project

---
+++ src/components/Burger/_burger.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Burger = () => {
+  
+   return (
+
+      <div className="burger">
+         <div className='burger__line'></div>
+         <div className='burger__line'></div>
+         <div className='burger__line'></div>
+      </div>
+
+   );
+
+}
+
+export default Burger
